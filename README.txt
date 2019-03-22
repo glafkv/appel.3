b22a9367f1106868fda37b39e40ac3992b80038c
Gabi <glafkv@mail.umsl.edu>
2019-03-21 20:15:12 -0500

    ):

--- README.txt
+++ README.txt
@@ -2,4 +2,14 @@ Project 3
 
 By: Gabi Appel
 
-Due: 03/19/19
+Due: 03/21/19
+
+Purpose: The purpose of this project is to familiarize ourselves with semaphores. It will read a file into shared memory and fork off processes. The child will decided if it's a palindrome or not, then send it to the proper output file. 
+
+How to Run:
+	
+	./master -h 		//pulls up the help menu
+	./master -f inputfile 	//specifies the file 
+
+Issues:
+	I think I got semaphores started... Unforunately, I have a difficult time putting things together properly and I couldn't figure out how to make it work. I don't know what I'm doing ): 
--- master.c
+++ master.c
@@ -14,10 +14,15 @@
 #define shared_key 8675309
 #define SEM_NAME "/makeSem"
 
-int shmid;
-char *shmaddr;
-sem_t *sem;
+sem_t mutex;
 
+void* thread(void* arg){
+	sem_wait(&mutex);
+	printf("Entered\n");
+	sleep(4);
+	printf("Exiting\n");
+	sem_post(&mutex);
+}
 int main(int argc, char *argv[])
 {
 	
@@ -31,7 +36,15 @@ int main(int argc, char *argv[])
 	FILE *outfptr1 = fopen(palinFile, "w");
 	FILE *outfptr2 = fopen(noPalinFile, "w");
 	
-
+	//Semaphore section
+	sem_init(&mutex, 0,1);
+	pthread_t t1,t2;
+	pthread_create(&t1, NULL, thread, NULL);
+	sleep(2);
+	pthread_create(&t2,NULL,thread,NULL);
+	pthread_join(t1,NULL);
+	pthread_join(t2,NULL);
+	sem_destroy(&mutex);
 	while((choice = getopt(argc,argv, "hf:")) != -1){
 		switch(choice){
 			case 'h':
@@ -74,79 +87,9 @@ int main(int argc, char *argv[])
 		exit(EXIT_FAILURE);
 	}
 	
-	/*char str1[80], str2[80];
-	int begin,middle,end, length = 0;
-	//Doesn't take number palindromes
-	 while(fgets(str1, sizeof(str1),infptr) != NULL){
-		//token=strtok(str1,s);
-		str1[strlen(str1)-1] = '\0';
-		while(str1[length]!='\0')
-			length++;
-		
-		end = length-1;
-		middle = length/2;
-		for(begin = 0; begin < middle; begin++){
-			if(str1[begin]!=str1[end]){
-				fprintf(outfptr2, "Not: %s\n", str1);
-				break;
-			}
-			end--;
-		}
-		if(begin==middle){
-			fprintf(outfptr1,"Palindrome: %s\n", str1);
-		}
-		
-	}*/
 	
-	//Create the semaphor
-	char parameter1 [12];
-	char *ptr;
-	int array[3];
+	
 
-	if((shmid = shmget(shared_key,1024,IPC_CREAT | IPC_EXCL | 0666)) < 0)
-	{
-		perror("shmget: ");
-		exit(1);
-	}
-	
-	shmaddr = (char *) shmat (shmid, 0,0);
-	ptr = shmaddr + sizeof (array);
-	
-	array[0] = sprintf(ptr, "0") +1;
-	ptr += array[0];
-	array[1] = sprintf(ptr, "74") +1;
-	ptr += array[1];
-	array[2] = sprintf(ptr, "932") +1;
-
-	memcpy(shmaddr, &array, sizeof (array));
-
-	sem = sem_open(SEM_NAME, O_CREAT | O_EXCL, S_IWUSR | S_IRUSR | S_IRGRP | S_IROTH, 0666, 1);
-	
-	if(sem == SEM_FAILED){
-		shmdt(shmaddr);
-		shmctl(shmid, IPC_RMID,NULL);
-		
-		sem_unlink(SEM_NAME);
-		sem_close(sem);
-		perror("Semaphore failure.");
-		
-		exit(EXIT_FAILURE);	
-	}
-	sprintf(parameter1, "%d", shmid);
-	
-	if(fork()==0){
-		execlp("./palin", "./palin", parameter1, (char *)NULL);
-		fprintf(stderr, "Failed to exec to palin.\n");
-	}
-	sleep(1);
-	sem_wait(sem);
-	printf("master.c is using the semaphore");
-	sem_post(sem);
-	
-	shmdt(shmaddr);
-	shmctl(shmid, IPC_RMID,NULL);
-	sem_close(sem);
-	sem_unlink(SEM_NAME);
 	//close the files
 	fclose(infptr);
 	fclose(outfptr1);
